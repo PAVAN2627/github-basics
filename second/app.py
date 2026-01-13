7dffc7f8ceafa3c4a8aa3a1d6bd0801cbfe28aab
PAVAN2627 <pavanmali0281@mail.com>
2026-01-13 18:36:40 +0000

    backend added

--- second/app.py
+++ second/app.py
@@ -13,6 +13,8 @@ app = Flask(__name__)
 client = MongoClient(MONGO_URI)
 db = client["testdb"]
 collection = db["users"]
+todo_collection = db["todos"]
+
 
 @app.route("/", methods=["GET", "POST"])
 def form():
@@ -35,6 +37,18 @@ def api():
     with open("second/api_data.json") as f:
         data = json.load(f)
     return jsonify(data)
+@app.route("/submittodoitem", methods=["POST"])
+def submit_todo_item():
+    item_name = request.form.get("itemName")
+    item_description = request.form.get("itemDescription")
+
+    todo_collection.insert_one({
+        "itemName": item_name,
+        "itemDescription": item_description
+    })
+
+    return "To-Do Item Saved Successfully"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
